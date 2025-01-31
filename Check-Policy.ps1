83b0d0ef3bee2bb10892845fb45ce360940f1ab0
Brandon-Baker11 <140644499+Brandon-Baker11@users.noreply.github.com>
2025-01-31 17:25:43 -0500

    Add files via upload

---
+++ Check-Policy.ps1
@@ -0,0 +1,25 @@
+﻿function Check-Policy {
+    param(
+        [String][Parameter(Mandatory=$True,Position=0)] $ComputerName
+        )
+        Import-Module ActiveDirectory
+
+        if (Test-Connection $ComputerName -Quiet) {
+            if (Test-Path -Path "\\$computername\c$\Windows\System32\GroupPolicy\Machine\Registry.pol") {
+                $macpolicy =  Get-ChildItem "\\$ComputerName\c$\Windows\System32\GroupPolicy\Machine\Registry.pol" | Select-Object LastWriteTime
+                $macdate = $macpolicy.LastWriteTime.ToShortDateString()
+                if ($macdate -le (Get-Date).AddDays(-7)) {
+                    Write-Host "Updating this computer's Group Policy..."
+                    Invoke-Command -ComputerName $ComputerName -ScriptBlock {gpupdate /force}
+                    }
+                    else {Write-Host "This computer's Group Policy was last updated $macdate"}
+                }
+                else {
+                Write-Host "File not found, refreshing policy."
+                Invoke-Command -ComputerName $ComputerName -ScriptBlock {gpupdate /force}
+                }
+        }
+        else {Write-Host "This computer is offline."}
+}
+
+
